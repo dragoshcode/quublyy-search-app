198b5ea631174dc97b8cc8fb2e0873738d275a52
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-24 18:39:16 +0200

    Enable dark mode toggle

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
+export const Navbar = ({ darkTheme, setDarkTheme }) => {
+  return (
+    <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200'>
+      <div className='flex justify-between items-center space-x-5 w-screen'>
+        <Link to='/'>
+          <img
+            src={logo}
+            alt='logo'
+            className='bg-blue-500 rounded w-40 px-2 dark:bg-gray-600'
+          />
+        </Link>
+        <button
+          type='button'
+          onClick={() => setDarkTheme(!darkTheme)}
+          className='text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg'
+        >
+          {darkTheme ? 'Light 💡' : 'Dark 🌕'}
+        </button>
+      </div>
+    </div>
+  );
+};
