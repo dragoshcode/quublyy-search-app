49132c0fe6ef082033d24332ea65ffaf9fe1ddd5
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-27 14:12:03 +0200

    Fix style problems x2

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ const App = () => {
           <Route path='/news' element={<Results />} />
           <Route path='/videos' element={<Results />} />
         </Routes>
-        <Footer />
       </div>
     </div>
   );
