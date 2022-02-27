bffc378ec81c8e7fb753dff24da72045f349c5d7
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-27 14:10:21 +0200

    Fix style problems

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-  return (
-    <div className='text-center p-10 mt-10 border-t fixed bottom-0 dark:border-gray-700 border-gray-200 w-full'>
-      <p className='text-center mx-auto'>2022 Quublyy, Inc</p>
-    </div>
-  );
-};
